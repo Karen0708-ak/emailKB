d3448948042a923285191d3d1ebb057e1e925565
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-30 08:26:48 -0500

    Se agregaron archivos para poder subir a render

--- emailKB/settings.py
+++ emailKB/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'emailKB.wsgi.application'
 
 DATABASES = {
     'default': {
+        
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'correoDb.sqlite3',
     }
@@ -121,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'emailKB/static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 MEDIA_URL = '/media/'
